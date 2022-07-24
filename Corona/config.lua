bcd1af8b818efe9c589e123b9f7184328ea2e988
Ansh3ll <ansh3ll@protonmail.com>
2022-07-24 22:06:18 +0300

    Simple sample

---
+++ Corona/config.lua
@@ -0,0 +1,15 @@
+--
+-- For more information on config.lua see the Project Configuration Guide at:
+-- https://docs.coronalabs.com/guide/basics/configSettings
+--
+
+application =
+{
+	content =
+	{
+		width = 600,
+		height = 1000,
+		scale = "letterbox",
+		fps = 60,
+	},
+}
---
+++ Corona/main.lua
@@ -0,0 +1,76 @@
+local gfxe = require('plugin.gfxe')
+
+local file = io.open(system.pathForFile('fonts/Inconsolata-Regular.ttf'), 'rb')
+local font = file:read('*a')
+file:close()
+
+-------------------------------------------------------------------
+
+local svg1 = gfxe.newScalableImage(
+    {
+        filename = 'images/h350.svg'
+    },
+    {
+        fontData = font,
+        fontFamily = 'Inconsolata'
+    }
+)
+svg1.x, svg1.y = display.contentCenterX, display.contentCenterY
+
+-------------------------------------------------------------------
+
+local sizing = gfxe.newSizing()
+sizing.aspect = gfxe.ZOOM_BY_RATIO
+sizing.zoom = 2
+
+local svg2 = gfxe.newScalableImage(
+    {
+        filename = 'images/svg_logo.svg'
+    },
+    {
+        sizing = sizing:raw()
+    }
+)
+svg2.x, svg2.y = display.contentCenterX, display.contentCenterY
+
+-------------------------------------------------------------------
+
+local qoi = gfxe.newStaticImage(
+    {
+        filename = 'images/qoi_logo.qoi',
+        width = 224, height = 110
+    }
+)
+qoi.x, qoi.y = 115, 58
+
+-------------------------------------------------------------------
+
+local webp1 = gfxe.newStaticImage(
+    {
+        filename = 'images/compass.webp',
+    }
+)
+webp1.x, webp1.y = 847, 153
+
+-------------------------------------------------------------------
+
+local webp2 = gfxe.newAnimatedImage(
+    {
+        filename = 'images/banana.webp',
+        width = 200, height = 200,
+        loop = true
+    }
+)
+webp2.x, webp2.y = 103, 497
+webp2:play()
+
+-------------------------------------------------------------------
+
+local webp3 = gfxe.newAnimatedImage(
+    {
+        filename = 'images/animated.webp',
+        width = 200, height = 200
+    }
+)
+webp3.x, webp3.y = 897, 497
+webp3:play()
